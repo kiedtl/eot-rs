8dc75f972b7849f0a55fa2c603a3750a46da6bc6
Kiëd Llaentenn <kiedtl@tilde.team>
2026-06-14 21:54:32 -0400

    chore: remove unused code

--- rust/eot/src/libeot.rs
+++ rust/eot/src/libeot.rs
@@ -1,9 +1,8 @@
 pub use crate::core::*;
 use crate::{EOT, writeFontFile};
 
-// pub const EOT_WARN: ::core::ffi::c_int = 1000 as ::core::ffi::c_int;
-pub const TTEMBED_TTCOMPRESSED: ::core::ffi::c_int = 0x4 as ::core::ffi::c_int;
-pub const TTEMBED_XORENCRYPTDATA: ::core::ffi::c_int = 0x10000000 as ::core::ffi::c_int;
+const TTEMBED_TTCOMPRESSED: ::core::ffi::c_int = 0x4 as ::core::ffi::c_int;
+const TTEMBED_XORENCRYPTDATA: ::core::ffi::c_int = 0x10000000 as ::core::ffi::c_int;
 
 // pub unsafe extern "C" fn EOTprintError(mut error: EOTError, mut out: *mut FILE) {
 //     match error as ::core::ffi::c_uint {
@@ -53,36 +52,7 @@ pub const TTEMBED_XORENCRYPTDATA: ::core::ffi::c_int = 0x10000000 as ::core::ffi
 //     };
 // }
 
-// pub unsafe extern "C" fn EOT2ttf_file(
-//     mut font: *const u8,
-//     mut fontSize: ::core::ffi::c_uint,
-//     mut metadataOut: *mut EOTMetadata,
-//     mut out: *mut FILE,
-// ) -> EOTError {
-//     let mut result: EOTError = EOTfillMetadata(font, fontSize, metadataOut);
-//     if result as ::core::ffi::c_uint >= EOT_WARN as ::core::ffi::c_uint {
-//         EOTprintError(result, stderr);
-//     } else if result as ::core::ffi::c_uint
-//         != EOT_SUCCESS as ::core::ffi::c_int as ::core::ffi::c_uint
-//     {
-//         return result
-//     }
-//     let mut writeResult: EOTError = writeFontFile(
-//         font.offset((*metadataOut).fontDataOffset as isize),
-//         (*metadataOut).fontDataSize as ::core::ffi::c_uint,
-//         (*metadataOut).flags & TTEMBED_TTCOMPRESSED as u32 != 0,
-//         (*metadataOut).flags & TTEMBED_XORENCRYPTDATA as u32 != 0,
-//         out,
-//     );
-//     if writeResult as ::core::ffi::c_uint
-//         != EOT_SUCCESS as ::core::ffi::c_int as ::core::ffi::c_uint
-//     {
-//         return writeResult;
-//     }
-//     return EOT_SUCCESS;
-// }
-
-pub unsafe fn EOT2ttf_buffer(data: &[u8]) -> Result<(EOTMetadata, Vec<u8>), Error> {
+pub fn EOT2ttf_buffer(data: &[u8]) -> Result<(EOTMetadata, Vec<u8>), Error> {
     let meta = EOT::read_metadata(data)?;
     let fontOut = writeFontFile::writeFontBuffer(
         &data[meta.fontDataOffset as usize..(meta.fontDataOffset + meta.fontDataSize) as usize],
